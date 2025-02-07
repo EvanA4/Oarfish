629451f35d2df84b0e7fbbef30b71f63b98e9ed3
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2025-02-06 22:48:46 -0500

    added parsing of other fields in FEN string and error handling

--- src/position.cpp
+++ src/position.cpp
@@ -4,10 +4,11 @@
 #include "position.hpp"
 
 
-void Position::fillBitboards(std::string &fen) {
+void Position::parseFEN(std::string &fen) {
     int len = fen.length();
     int currIdx = 56; // using Layout 3 for bitboards, FEN starts in top left
 
+    int endPosIdx;
     for (int i = 0; i < len; ++i) {
 
         // if empty space(s), add to currIdx
@@ -25,6 +26,7 @@ void Position::fillBitboards(std::string &fen) {
             if (currIdx != 8) {
                 throw std::runtime_error("Illegal position encoding in FEN string " + fen + ".");
             }
+            endPosIdx = i + 1;
             break;
         }
 
@@ -66,6 +68,86 @@ void Position::fillBitboards(std::string &fen) {
             throw std::runtime_error(errstr);
         }
     }
+
+    // use endPosIdx to parse the rest of these values:
+    // Example: "w KQkq - 0 1"
+    whiteTurn = fen[endPosIdx] == 'w';
+    if (!whiteTurn && fen[endPosIdx] != 'b') {
+        throw std::runtime_error("Invalid player-to-move in FEN string \"" + fen + "\".");
+    }
+
+    // short castleRights;
+    castleRights = 0;
+    int i = endPosIdx + 2;
+    while (1) {
+        if (fen[i] == ' ') {
+            break;
+        }
+
+        if (i == len) {
+            throw std::runtime_error("FEN string \"" + fen + "\" terminates unexpectedly.");
+            break;
+        }
+
+        switch (fen[i]) {
+            case 'K':
+                if (castleRights & 1) {
+                    throw std::runtime_error("Illegal repeat of character in castle field.");
+                }
+                castleRights |= 1;
+                break;
+            case 'Q':
+                if (castleRights & 2) {
+                    throw std::runtime_error("Illegal repeat of character in castle field.");
+                }
+                castleRights |= 2;
+                break;
+            case 'k':
+                if (castleRights & 4) {
+                    throw std::runtime_error("Illegal repeat of character in castle field.");
+                }
+                castleRights |= 4;
+                break;
+            case 'q':
+                if (castleRights & 8) {
+                    throw std::runtime_error("Illegal repeat of character in castle field.");
+                }
+                castleRights |= 8;
+                break;
+            case '-':
+                break;
+            default:
+                std::string errstr = "Invalid character \"a\" in FEN castle rights.";
+                errstr[19] = fen[i];
+                throw std::runtime_error(errstr);
+                break;
+        }
+
+        ++i;
+    }
+
+    // short passantSq;
+    ++i;
+    if (i == len) {
+        throw std::runtime_error("FEN string \"" + fen + "\" terminates unexpectedly.");
+    }
+
+    if (fen[i] == '-') {
+        passantSq = -1;
+        i += 2;
+    } else {
+        if (i + 2 >= len) {
+            throw std::runtime_error("FEN string \"" + fen + "\" terminates unexpectedly.");
+        }
+        passantSq = 8 * (fen[i + 1] - '1') + (fen[i] - 'a');
+        i += 3;
+    }
+
+    // short halfMove;
+    // short fullMove;
+    if (sscanf(&fen[i], "%hd %hd", &halfMove, &fullMove) != 2) {
+        throw std::runtime_error("FEN string \"" + fen + "\" has invalid en passant, half move, or full move fields.");
+    }
 }
 
 
@@ -80,7 +162,7 @@ void Position::printBitBoard(ulong bb) {
 }
 
 
-void Position::printBitBoards() {
+void Position::printState() {
     std::cout << "White pieces:" << std::endl;
     printBitBoard(team_bbs[0]);
     std::cout << std::endl << "Black pieces:" << std::endl;
@@ -93,10 +175,20 @@ void Position::printBitBoards() {
         printBitBoard(type_bbs[i]);
         std::cout << std::endl;
     }
+
+    std::cout << "whiteTurn: " << whiteTurn << std::endl;
+    std::cout << "castleRights: 0b"
+        << ((castleRights >> 3) & 1)
+        << ((castleRights >> 2) & 1)
+        << ((castleRights >> 1) & 1)
+        << (castleRights & 1) << std::endl;
+    std::cout << "passantSq: " << passantSq << std::endl;
+    std::cout << "halfMove: " << halfMove << std::endl;
+    std::cout << "fullMove: " << fullMove << std::endl;
 }
 
 
 Position::Position(std::string &fen) {
-    fillBitboards(fen);
-    printBitBoards();
+    parseFEN(fen);
+    printState();
 }
--- src/position.hpp
+++ src/position.hpp
@@ -8,16 +8,22 @@ class Position {
     public:
         ulong team_bbs[2] = {0ULL, 0ULL};
         ulong type_bbs[6] = {0ULL, 0ULL, 0ULL, 0ULL, 0ULL, 0ULL};
+        bool whiteTurn;
+        short castleRights;
+        short passantSq;
+        short halfMove;
+        short fullMove;
 
     private:
         const std::set<char> PIECES = {
             'P', 'R', 'N', 'B', 'Q', 'K',
             'p', 'r', 'n', 'b', 'q', 'k'
         };
-        void fillBitboards(std::string &fen);
+        void parseFEN(std::string &fen);
 
     public:
         void printBitBoard(ulong bb);
-        void printBitBoards();
+        void printState();
+        
         Position(std::string &fen);
 };
